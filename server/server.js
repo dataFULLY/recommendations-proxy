9ee552dbc8b45b863b8a9508e1f1c452ce08e0e3
Justin Wang <justin@Justins-MacBook-Pro.local>
2019-09-21 10:58:09 -0700

    Set up proxy for deployment

--- server/server.js
+++ server/server.js
@@ -6,7 +6,16 @@ const app = express();
 const port = 3000;
 
 app.use(morgan('tiny'));
-app.use('/api/listings/:listing', express.static('public'));
+app.use('/listing/:listing', express.static('public'));
 app.use(express.static('public'));
+
+
+app.use('/api/listing/:listing', (req, res) => {
+  let { id } = req.params;
+  if (!id) {
+    id = 0;
+  }
+  res.redirect(`http://ec2-54-200-65-2.us-west-2.compute.amazonaws.com:3005/api/listing/${id}`);
+});
 
 app.listen(port);
